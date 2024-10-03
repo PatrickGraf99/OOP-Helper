3a6149f448db9b94e994e9f292f9a8f708e17fa0
PatrickGraf99 <patrickgraf18@outlook.com>
2024-10-03 02:23:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# load .env
 load_dotenv()
 
+# secret token to access bot (Never share this)
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# select intents the bot needs in order to work
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
@@ -16,7 +19,8 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
+# Print to console when bot is ready and send Online message to channel
+# TODO: filter channel ID dynamically/provide way to change channelID
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -26,7 +30,7 @@ async def on_ready():
     if channel:
         await channel.send("Hi! The OOP-Helper Bot is online now!")
 
-
+# Test command
 @bot.command(name="greet")
 async def greet(ctx):
     await ctx.send(f"Hello {ctx.author.mention}")
