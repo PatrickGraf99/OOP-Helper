0128bbf040219d4a00a98f11a2c59afa658e12af
PatrickGraf99 <patrickgraf18@outlook.com>
2024-10-04 02:48:20 +0200

    Create dm-channel command
    Created command so that a channel for the bot to redirect DMs to can be selected

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# TODO Refactor so Bot is a class???
+
 # load .env
 load_dotenv()
 
@@ -17,7 +19,12 @@ intents.messages = True
 intents.message_content = True
 intents.guilds = True
 
+# If channel ID is from a Server without the bot, bot.get_channel apparently returns None
+dm_channel_id = 1291174504382595206
+print(dm_channel_id)
+
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 # Print to console when bot is ready and send Online message to channel
 # TODO: filter channel ID dynamically/provide way to change channelID
@@ -25,16 +32,48 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-    channel = bot.get_channel(1291174504382595206)
+    channel = bot.get_channel(dm_channel_id)
 
     if channel:
         await channel.send("Hi! The OOP-Helper Bot is online now!")
 
+
+# Credits: https://stackoverflow.com/questions/65062678/how-to-make-my-bot-forward-dms-sent-to-it-to-a-channel
+@bot.event
+async def on_message(message):
+    # Logic for when Bot receives a DM
+    if isinstance(message.channel, discord.DMChannel):
+        target_channel = bot.get_channel(dm_channel_id)
+        print(f'Forwarding message to channel ID {target_channel.id}')
+        # Send content of DM back to specified dm-channel
+        await target_channel.send(message.content)
+    # Processing message so commands will work
+    await bot.process_commands(message)
+
+
+# Set Channel for Questions
+@bot.command(name='dm-channel', help='Sets the channel where bot DMs will be redirected to')
+async def set_dm_channel(ctx, channel_name=''):
+    # Send syntax hint if no channel name was specified
+    if channel_name == '':
+        await ctx.send('Command Syntax: !dm-channel <channel_name>')
+        return
+    guild = ctx.guild
+    channel = discord.utils.get(guild.text_channels, name=channel_name)
+    # Send error hint if channel does not exist
+    if channel is None:
+        await ctx.send(f'Found no channel \"{channel_name}\"')
+        return
+    # Use global so dm_channel_id is properly updated and set new ID
+    global dm_channel_id
+    dm_channel_id = channel.id
+    await ctx.send(f'Set channel where DMs will be redirected to to {dm_channel_id}')
+
+
 # Test command
-@bot.command(name="greet")
+@bot.command(name='greet')
 async def greet(ctx):
     await ctx.send(f"Hello {ctx.author.mention}")
 
 
 bot.run(TOKEN)
-
